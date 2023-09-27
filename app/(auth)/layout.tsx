cad727211a58222f6e4a308cf10ad5a1bb19d779
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-27 03:14:19 +0100

    Adding in layout for route group (as we don't want to see header or footer when signing in/up

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+export const metadata = {
+    title: 'Threads',
+    description: 'A next.js Meta threads application'
+}
+
+export default function RootLayout({ 
+    children 
+}: { 
+    children:React.ReactNode
+}) {
+    return
+}
+
+
+
+
+
+
+//IGNORE: Comments here are for my own learning //
+
+/**
+ * metadata object contains the two properties which are used for SEO purposes.
+ * 
+ * RootLayout is a React functional component that
+ * takes the single prop 'children'.
+ * 
+ * The prop is expected to be a type of React.ReactNode which is 
+ * a type provided by react for representing the contents of a
+ * react component. 
+ */
