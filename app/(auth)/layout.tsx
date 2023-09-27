6fed12a87159344d6e5f6d1fddb35c7463dfdba4
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-27 03:20:13 +0100

    Added in const inter for font and added in some styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,31 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import '../globals.css';
+
 export const metadata = {
     title: 'Threads',
     description: 'A next.js Meta threads application'
 }
+
+const inter = Inter({ subsets: ["latin"]})
 
 export default function RootLayout({ 
     children 
 }: { 
     children:React.ReactNode
 }) {
-    return
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1` }>
+                    {children}
+
+                </body>
+            </html>
+
+        </ClerkProvider>
+
+    ) 
 }
 
 
