90f30788791f3ff38b5ee2879d9bcc851278ad85
shoaiblatif22 <shoaiblatif11@gmail.com>
2023-09-27 15:13:57 +0100

    Debugged layout.tsx as it wasn't compiling correctly. Added in a clerk provider which was supposed to be wrapped around the html. Copied and pasted start code from clerk docs to sign-in and sign-up pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <main>
-      <h1> Threads </h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
-
